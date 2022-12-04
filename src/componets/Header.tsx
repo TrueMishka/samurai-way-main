67aee1c5784fe2a4b63b85ae6327fe8878cf7216
TrueMishka <Mixxxa32123@gmail.com>
2022-12-04 18:41:34 +0300

    Lesson 12. Componets

---
+++ src/componets/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src={'https://e7.pngegg.com/pngimages/462/393/png-clipart-others-chicken-logo.png'}/>
+        </header>
+    );
+}
+
+export default Header;
