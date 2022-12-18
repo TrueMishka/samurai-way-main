0353b5b704608041db822ec580dde454dc346d97
TrueMishka <Mixxxa32123@gmail.com>
2022-12-18 13:31:52 +0300

    Lesson 19. Route, browser-router

--- src/componets/Dialogs/Dialogs.tsx
+++ src/componets/Dialogs/Dialogs.tsx
@@ -3,8 +3,29 @@ import classes from "./Dialogs.module.css";
 
 export const Dialogs = () => {
     return(
-        <div>
-            Dialogs
+        <div className={classes.dialogs}>
+            <div className={classes.dialogItems}>
+                <div className={classes.dialog + ' ' + classes.active}>
+                    Mishka
+                </div>
+                <div className={classes.dialog}>
+                    Vizix
+                </div>
+                <div className={classes.dialog}>
+                    Kibot
+                </div>
+                <div className={classes.dialog}>
+                    Burn
+                </div>
+                <div className={classes.dialog}>
+                    Zveklas
+                </div>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.message}>Hi</div>
+                <div className={classes.message}>How are you?</div>
+                <div className={classes.message}>yo!</div>
+            </div>
         </div>
     );
 }
